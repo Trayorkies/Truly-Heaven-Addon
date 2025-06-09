87d0f64531eb883918d0a91768e278270e58c6a7
PlanetTeamSpeak <PlanetTeamSpeakk@users.noreply.github.com>
2025-06-09 19:13:40 +0200

    Initial commit

---
+++ common/src/main/java/com/example/myjaddon/ExampleMod.java
@@ -0,0 +1,9 @@
+package com.example.myjaddon;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "myjaddon";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
